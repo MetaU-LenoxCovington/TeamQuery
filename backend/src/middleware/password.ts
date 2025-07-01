9333b4fc30f7dafeac8b7418590760feb144d4c0
Lenox 'A'ali'i Covington <lenox@meta.com>
2025-07-01 11:18:11 -0700

    create hash and verify functions for passwords

---
+++ backend/src/middleware/password.ts
@@ -0,0 +1,20 @@
+import * as argon2 from 'argon2';
+
+export class PasswordUtils {
+  static async hash(password: string): Promise<string> {
+    return argon2.hash(password, {
+      type: argon2.argon2id,
+      memoryCost: 2 ** 16,
+      timeCost: 3,
+      parallelism: 1,
+    });
+  }
+
+  static async verify(hashedPassword: string, plainPassword: string): Promise<boolean> {
+    try {
+      return await argon2.verify(hashedPassword, plainPassword);
+    } catch (error) {
+      return false;
+    }
+  }
+}
