74a5626d0c706f33eed992df76c23cd7414812af
Lenox 'A'ali'i Covington <lenox@meta.com>
2025-07-10 00:14:03 -0700

    Export hooks from index file to make import simpler

---
+++ frontend/src/hooks/chat/index.ts
@@ -0,0 +1,6 @@
+export { useChat } from './useChat';
+export { useMessageInput } from './useMessageInput';
+export { useSourceExpansion } from './useSourceExpansion';
+export type { UseChatReturn } from './useChat';
+export type { UseMessageInputReturn, UseMessageInputOptions } from './useMessageInput';
+export type { UseSourceExpansionReturn } from './useSourceExpansion';
